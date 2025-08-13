08c29f71e79ec31d78f36eeef33e643a4fc84342
isaackhlam <isaac.lamkh@gmail.com>
2025-08-13 16:59:44 +0800

    feat: add dnn classifier

--- py-project/src/dnn.py
+++ py-project/src/dnn.py
@@ -193,6 +193,201 @@ def build_config(
 
 
 @dnn_app.command()
+def classification(
+    train_data_path: str = "example/data/classification_train.csv",
+    test_data_path: str = "example/data/classification_test.csv",
+    output_result_path: str = "example/data/classification_dnn_output.csv",
+    preview_prediction_result: bool = False,
+    label_name: str = "label",
+    hidden_layer_sizes: Optional[str] = typer.Option(
+        None,
+        help="The ith element represents the number of neurons in the ith hidden layer. Default: (100,)",
+        show_default=False,
+    ),
+    activation: Optional[Activation] = typer.Option(
+        None,
+        help="Activation function for the hidden layer. Default: relu",
+        show_default=False,
+    ),
+    solver: Optional[Solver] = typer.Option(
+        None,
+        help="The solver for weight optimization. Default 'adam'",
+        show_default=False,
+    ),
+    alpha: Optional[float] = typer.Option(
+        None,
+        help="Strength of the L2 regularization term. Default: 0.0001",
+        show_default=False,
+    ),
+    batch_size: Optional[str] = typer.Option(
+        None,
+        help="Size of minibatches for stochastic optimizer. Default: 'auto'",
+        show_default=False,
+    ),
+    learning_rate: Optional[LearningRate] = typer.Option(
+        None,
+        help="Learning rate schedule for weight updates Default: 'constant'",
+        show_default=False,
+    ),
+    learning_rate_init: Optional[float] = typer.Option(
+        None, help="The initial learning rate used. Default: 0.001", show_default=False
+    ),
+    power_t: Optional[float] = typer.Option(
+        None,
+        help="The exponent for inverse scaling learning rate. Default: 0.5",
+        show_default=False,
+    ),
+    max_iter: Optional[int] = typer.Option(
+        None, help="Maximum number of iterations. Default: 200", show_default=False
+    ),
+    shuffle: Optional[bool] = typer.Option(
+        None,
+        help="Whether to shuffle samples in each iteration. Default: True",
+        show_default=True,
+    ),
+    random_state: Optional[int] = typer.Option(
+        None,
+        help="Determines random number generation. Default: None",
+        show_default=False,
+    ),
+    tol: Optional[float] = typer.Option(
+        None, help="Tolerance for the optimization. Default: 1e-4", show_default=False
+    ),
+    verbose: Optional[bool] = typer.Option(
+        None,
+        help="Whether to print progress messags to stdout. Default: False",
+        show_default=False,
+    ),
+    warm_start: Optional[bool] = typer.Option(
+        None,
+        help="Whether reuse the solution of the previous call to fit as initialization. Default: False",
+        show_default=False,
+    ),
+    momentum: Optional[float] = typer.Option(
+        None,
+        help="Momentum for gradient descent update. Default: 0.9",
+        show_default=False,
+    ),
+    nesterovs_momentum: Optional[bool] = typer.Option(
+        None,
+        help="Whether to use Nesterov's Momentum. Default: True",
+        show_default=False,
+    ),
+    early_stopping: Optional[bool] = typer.Option(
+        None,
+        help="Whether to use early stopping to terminate training when validation score is not improving. Default: False",
+        show_default=False,
+    ),
+    validation_fraction: Optional[float] = typer.Option(
+        None,
+        help="The proportion of training data to set aside as validation set for early stopping. Default: 0.1",
+        show_default=False,
+    ),
+    beta_1: Optional[float] = typer.Option(
+        None,
+        help="Exponential decay rate for estimates of first moment vector in adam. Default: 0.9",
+        show_default=False,
+    ),
+    beta_2: Optional[float] = typer.Option(
+        None,
+        help="Exponential decay rate for estimates of second moment vector in adam. Default: 0.999",
+        show_default=False,
+    ),
+    epsilon: Optional[float] = typer.Option(
+        None,
+        help="Value for numerical stability in adam. Default: 1e-8",
+        show_default=False,
+    ),
+    n_iter_no_change: Optional[int] = typer.Option(
+        None,
+        help="Maximum number of epochs to not meet tol improvement. Default: 10",
+        show_default=False,
+    ),
+    max_fun: Optional[int] = typer.Option(
+        None,
+        help="Maximum number of function calls. Only used when solver='lbfgs'. Default: 15000",
+        show_default=False,
+    ),
+):
+
+    hidden_layer_sizes = coerce_value(
+        Config.__annotations__["hidden_layer_sizes"], hidden_layer_sizes
+    )
+    batch_size = coerce_value(Config.__annotations__["batch_size"], batch_size)
+
+    config = build_config(
+        loss=None,
+        hidden_layer_sizes=hidden_layer_sizes,
+        activation=activation,
+        solver=solver,
+        alpha=alpha,
+        batch_size=batch_size,
+        learning_rate=learning_rate,
+        learning_rate_init=learning_rate_init,
+        power_t=power_t,
+        max_iter=max_iter,
+        shuffle=shuffle,
+        random_state=random_state,
+        tol=tol,
+        verbose=verbose,
+        warm_start=warm_start,
+        momentum=momentum,
+        nesterovs_momentum=nesterovs_momentum,
+        early_stopping=early_stopping,
+        validation_fraction=validation_fraction,
+        beta_1=beta_1,
+        beta_2=beta_2,
+        epsilon=epsilon,
+        n_iter_no_change=n_iter_no_change,
+        max_fun=max_fun,
+    )
+    model = neural_network.MLPClassifier(
+        hidden_layer_sizes=config.hidden_layer_sizes,
+        activation=config.activation,
+        solver=config.solver,
+        alpha=config.alpha,
+        batch_size=config.batch_size,
+        learning_rate=config.learning_rate,
+        learning_rate_init=config.learning_rate_init,
+        power_t=config.power_t,
+        max_iter=config.max_iter,
+        shuffle=config.shuffle,
+        random_state=config.random_state,
+        tol=config.tol,
+        verbose=config.verbose,
+        warm_start=config.warm_start,
+        momentum=config.momentum,
+        nesterovs_momentum=config.nesterovs_momentum,
+        early_stopping=config.early_stopping,
+        validation_fraction=config.validation_fraction,
+        beta_1=config.beta_1,
+        beta_2=config.beta_2,
+        epsilon=config.epsilon,
+        n_iter_no_change=config.n_iter_no_change,
+        max_fun=config.max_fun,
+    )
+
+    logger.info("Loading training data.")
+    train_X, train_y = load_data(train_data_path, label_name)
+    logger.info("Training model.")
+    model.fit(train_X, train_y)
+    logger.info("Finished model training.")
+
+    logger.info("Loading testing data.")
+    test_X = load_data(test_data_path)
+    test_X = test_X[train_X.columns]
+    logger.info("Predicting test data")
+    predictions = model.predict(test_X)
+    test_pred = test_X.copy()
+    test_pred["prediction"] = predictions
+    test_pred.to_csv(output_result_path, index=False)
+    logger.info(f"Prediction Finished, result saved to {output_result_path}")
+
+    if preview_prediction_result is True:
+        print(test_pred)
+
+
+@dnn_app.command()
 def regression(
     train_data_path: str = "example/data/regression_train.csv",
     test_data_path: str = "example/data/regression_test.csv",
