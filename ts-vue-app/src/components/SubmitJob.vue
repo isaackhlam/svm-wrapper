22686cd21a1346450bc5e7e71db90839feba32d9
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2025-08-29 21:13:52 +0800

    wip: finish one basic form

--- ts-vue-app/src/components/SubmitJob.vue
+++ ts-vue-app/src/components/SubmitJob.vue
@@ -9,6 +9,25 @@ const initialValues = reactive({
     labelName: 'label',
     svmType: 'C',
     advanceOption: false,
+    penalty: 'l2',
+    dual: 'auto',
+    multiClass: 'ovr',
+    fitIntercept: true,
+    interceptScaling: 1.0,
+    kernel: 'rbf',
+    degree: 3,
+    gamma: '',
+    coef0: 0.0,
+    shirnking: true,
+    probability: false,
+    tol: 1e-3,
+    cacheSize: 200,
+    classWeight: null,
+    verbose: false,
+    maxIter: -1,
+    decisionFunctionShape: 'ovr',
+    breakTies: false,
+    randomState: null,
 });
 
 
@@ -61,9 +80,13 @@ const onFormSubmit = ({ valid }) => {
                   <RadioButton inputId="c" value="C" />
                   <label for="c">C</label>
                 </div>
-                <div class="p-field-radiobutton">
+                <div class="flex items-center gap-2">
                   <RadioButton inputId="nu" value="Nu" />
                   <label for="nu">Nu</label>
+                </div>
+                <div class="flex items-center gap-2">
+                  <RadioButton inputId="linear" value="Linear" />
+                  <label for="nu">Linear</label>
                 </div>
               </RadioButtonGroup>
             </Fieldset>
@@ -82,8 +105,138 @@ const onFormSubmit = ({ valid }) => {
               <ToggleSwitch name="advanceOption" />
             </div>
             <div v-show="$form.advanceOption?.value">
-              <label for="nuValue" class="font-bold block mb-2"> BB </label>
-              <InputNumber name="nuValue" fluid />
+              <Fieldset legend="Penalty">
+                <RadioButtonGroup name="penalty" class="flex flex-wrap gap-4">
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="l1" value="l1" />
+                    <label for="l1">l1</label>
+                  </div>
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="l2" value="l2" />
+                    <label for="l2">l2</label>
+                  </div>
+                </RadioButtonGroup>
+              </Fieldset>
+              <Fieldset legend="Dual">
+                <RadioButtonGroup name="dual" class="flex flex-wrap gap-4">
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="auto" value="auto" />
+                    <label for="auto">auto</label>
+                  </div>
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="true" value="true" />
+                    <label for="true">true</label>
+                  </div>
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="false" value="false" />
+                    <label for="false">false</label>
+                  </div>
+                </RadioButtonGroup>
+              </Fieldset>
+              <Fieldset legend="MultiClass">
+                <RadioButtonGroup name="multiClass" class="flex flex-wrap gap-4">
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="ovr" value="ovr" />
+                    <label for="ovr">ovr</label>
+                  </div>
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="crammer_singer" value="crammer_singer" />
+                    <label for="crammer_singer">crammer_singer</label>
+                  </div>
+                </RadioButtonGroup>
+              </Fieldset>
+              <div class="flex flex-col items-center gap-2">
+                <label for="fitIntercept" class="font-bold block mb-2"> Fit Intercept </label>
+                <ToggleSwitch name="fitIntercept" />
+              </div>
+              <div class="flex flex-col items-center gap-2">
+                <label for="interceptScaling" class="font-bold block mb-2"> Intercept Scaling </label>
+                <InputNumber name="interceptScaling" fluid />
+              </div>
+              <Fieldset legend="Kernel">
+                <RadioButtonGroup name="kernel" class="flex flex-wrap gap-4">
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="linear" value="linear" />
+                    <label for="linear">linear</label>
+                  </div>
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="poly" value="poly" />
+                    <label for="poly">polynomial</label>
+                  </div>
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="rbf" value="rbf" />
+                    <label for="rbf">rbf</label>
+                  </div>
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="sigmoid" value="sigmoid" />
+                    <label for="sigmoid">sigmoid</label>
+                  </div>
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="precomputed" value="precomputed" />
+                    <label for="precomputed">precomputed</label>
+                  </div>
+                </RadioButtonGroup>
+              </Fieldset>
+              <div v-show="$form?.kernel?.value === 'poly'">
+                <label for="degree" class="font-bold block mb-2"> Degree </label>
+                <InputNumber name="degree" fluid />
+              </div>
+              <div class="flex flex-col items-center gap-2">
+                <label for="gamma" class="font-bold block mb-2"> Gamma </label>
+                <InputNumber name="gamma" fluid />
+              </div>
+              <div class="flex flex-col items-center gap-2">
+                <label for="coef0" class="font-bold block mb-2"> coef0 </label>
+                <InputNumber name="coef0" fluid />
+              </div>
+              <div class="flex flex-col items-center gap-2">
+                <label for="shinrking" class="font-bold block mb-2"> Shirnking </label>
+                <ToggleSwitch name="shirnking" />
+              </div>
+              <div class="flex flex-col items-center gap-2">
+                <label for="probability" class="font-bold block mb-2"> Probability </label>
+                <ToggleSwitch name="probability" />
+              </div>
+              <div class="flex flex-col items-center gap-2">
+                <label for="tol" class="font-bold block mb-2"> tol </label>
+                <InputNumber name="tol"fluid />
+              </div>
+              <div class="flex flex-col items-center gap-2">
+                <label for="cacheSize" class="font-bold block mb-2"> Cache Size </label>
+                <InputNumber name="cacheSize" fluid />
+              </div>
+              <div class="flex flex-col items-center gap-2">
+                <label for="classWeight" class="font-bold block mb-2"> Class Weight </label>
+                <InputText name="classWeight" fluid />
+              </div>
+              <div class="flex flex-col items-center gap-2">
+                <label for="verbose" class="font-bold block mb-2"> Verbose </label>
+                <ToggleSwitch name="verbose" />
+              </div>
+              <div class="flex flex-col items-center gap-2">
+                <label for="maxIter" class="font-bold block mb-2"> Max Iteration </label>
+                <InputText name="maxIter" fluid />
+              </div>
+              <Fieldset legend="Decision Function Shape">
+                <RadioButtonGroup name="decisionFunctionShape" class="flex flex-wrap gap-4">
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="ovr" value="ovr" />
+                    <label for="ovr">ovr</label>
+                  </div>
+                  <div class="flex items-center gap-2">
+                    <RadioButton inputId="ovo" value="ovo" />
+                    <label for="ovo">ovo</label>
+                  </div>
+                </RadioButtonGroup>
+              </Fieldset>
+              <div class="flex flex-col items-center gap-2">
+                <label for="breakTies" class="font-bold block mb-2"> Break Ties </label>
+                <ToggleSwitch name="breakTies" />
+              </div>
+              <div class="flex flex-col items-center gap-2">
+                <label for="randomState" class="font-bold block mb-2"> Random State </label>
+                <InputText name="randomState" fluid />
+              </div>
             </div>
 
             <Button type="submit" severity="secondary" label="Submit" />
