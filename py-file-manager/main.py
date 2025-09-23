2bed53b454e2e0d6815dea1d3b355f58f95a04f3
isaackhlam <isaac.lamkh@gmail.com>
2025-09-23 17:49:41 +0800

    feat: add file getter

--- py-file-manager/main.py
+++ py-file-manager/main.py
@@ -4,6 +4,7 @@ import uuid
 from typing import Union
 
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
 from minio import Minio
 
 client = Minio(
@@ -49,6 +50,30 @@ async def upload(file: UploadFile = File(...), key: str = str(...)):
             part_size=10 * 1024 * 1024,
         )
         print(f"appended {result.object_name} object; etag: {result.etag}")
+    except Exception as ex:
+        print(traceback.format_exc())
+        return {
+            "statusCode": 400,
+            "body": ex,
+        }
+
+@app.get("/download/{key}")
+async def download(key: str):
+    filename = f"/{key}/output.csv"
+    try:
+        obj = client.get_object(
+            bucket_name,
+            filename,
+        )
+        stat = client.stat_object(bucket_name, filename)
+        content_type = stat.content_type or "application/octet-stream"
+
+        return StreamingResponse(
+            obj,
+            media_type=content_type,
+            headers={
+                "Content-Disposition": f'attachment; filename="{key}_output.csv"'
+            })
     except Exception as ex:
         print(traceback.format_exc())
         return {
