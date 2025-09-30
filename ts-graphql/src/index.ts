c11a17aab7fd123be5dd41dab9afe6ba08f777ed
isaackhlam <isaac.lamkh@gmail.com>
2025-09-30 17:51:09 +0800

    feat: using docker compose to start all services at once

--- ts-graphql/src/index.ts
+++ ts-graphql/src/index.ts
@@ -14,7 +14,7 @@ const main = async () => {
     context: baseContext,
   });
   const server = createServer(yoga);
-  server.listen(4000, () => {
+  server.listen(4000, '0.0.0.0', () => {
     console.info('Server is running on http://localhost:4000/graphql');
   });
 };
