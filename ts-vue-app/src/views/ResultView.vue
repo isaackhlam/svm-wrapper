1e3a514c77b19096f07f18fec6b3ac064843c928
isaackhlam <isaac.lamkh@gmail.com>
2025-10-02 19:55:00 +0800

    feat: download prediction csv from result page

--- ts-vue-app/src/views/ResultView.vue
+++ ts-vue-app/src/views/ResultView.vue
@@ -2,13 +2,16 @@
   import { ref, onMounted } from 'vue';
   import { useRoute } from 'vue-router';
   import { GraphQLClient, gql } from 'graphql-request';
+  import axios from 'axios';
 
   const endpoint = import.meta.env.VITE_GRAPHQL_ENDPOINT || "http://localhost:4000/graphql/";
+const fileEndpoint = import.meta.env.VITE_FILE_HANDLER_ENDPOINT || "http://localhost:8000/";
   const client = new GraphQLClient(endpoint);
   const route = useRoute();
   const jobId = route.params.jobId as string;
 
   const jobStatus = ref<string | null>(null);
+  const isDownloading = ref(false);
 
   // TODO: Separete import for qgl query
   const query = gql`
@@ -25,9 +28,50 @@
       jobStatus.value = "Error";
     }
   });
+
+  // TODO: Refactor this download logic
+  const downloadResult = async () => {
+    isDownloading.value = true;
+
+    try {
+      const response = await axios.get(`${fileEndpoint}download/${jobId}`,
+      {
+        reponseType: 'text',
+      }
+    );
+    const csvText = response.data;
+    const filename = "output.csv";
+
+    const blob = new Blob([csvText], { type: 'text/csv;charset=utf-8;' });
+    const blobUrl = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = blobUrl;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    URL.revokeObjectURL(blobUrl);
+  } catch (error) {
+    console.error('Download failed:', error);
+    alert('Failed to download the result file.');
+  } finally {
+    isDownloading.value = false;
+  }
+};
 </script>
 
 <template>
-  <p v-if="jobStatus">This is result page, Your job {{ jobId }} is {{ jobStatus }}</p>
-  <p v-else>Loading job status...</p>
+  <div>
+    <p v-if="jobStatus">This is result page, Your job {{ jobId }} is {{ jobStatus }}</p>
+    <p v-else>Loading job status...</p>
+
+    <Button
+      v-if="jobStatus === 'FINISHED'"
+      :label="isDownloading ? 'Downloading...' : 'Download Result'"
+      icon="pi pi-download"
+      @click="downloadResult"
+      class="p-button-success mt-3"
+      :disabled="isDownloading"
+    />
+  </div>
 </template>
