1963dc34bd2a647199804bc0a6fb5c080afcd8a6
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2025-10-02 16:18:04 +0800

    feat: add skeleton result page

---
+++ ts-vue-app/src/views/ResultView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import { useRoute } from 'vue-router';
+  const route = useRoute();
+  const jobId = route.params.jobId;
+</script>
+
+<template>
+  <p>This is result page, Your job {{ jobId }} is processing</p>
+</template>
